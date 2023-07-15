d4159fc22702dc6f7214e8eea3ce9affc1d34d78
vicentec <vcersosimo93@gmail.com>
2023-07-15 15:57:23 -0300

    p

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -6,7 +6,9 @@ cd out
 #aws s3 sync ./_next s3://$S3_ORIGIN_BUCKET/_next --exclude "data/*" --metadata-directive 'REPLACE' --cache-control max-age=31536000,public,immutable --delete
 #aws s3 sync ./_next/data s3://$S3_ORIGIN_BUCKET/_next/data --metadata-directive 'REPLACE' --cache-control max-age=3600,must-revalidate --delete
 
-aws s3 sync ./dist/ s3://$S3_ORIGIN_BUCKET/dist/ --metadata-directive 'REPLACE' --cache-control max-age=3600,must-revalidate --delete
+aws s3 sync ./dist/index.html s3://$S3_ORIGIN_BUCKET/dist/index.html --metadata-directive 'REPLACE' --cache-control max-age=3600,must-revalidate --delete
+aws s3 sync ./dist/react-mf-root-config.js s3://$S3_ORIGIN_BUCKET/dist/react-mf-root-config.js --metadata-directive 'REPLACE' --cache-control max-age=3600,must-revalidate --delete
+aws s3 sync ./dist/react-mf-root-config.js.map s3://$S3_ORIGIN_BUCKET/dist/react-mf-root-config.js.map --metadata-directive 'REPLACE' --cache-control max-age=3600,must-revalidate --delete
 
 # Sync HTML and other files with no cache
 #aws s3 sync ./ s3://$S3_ORIGIN_BUCKET --exclude "common/favicons/*" --exclude "_next/*" --exclude "common/assets/*" --include "common/favicons/site.webmanifest" --metadata-directive 'REPLACE' --cache-control no-cache,no-store,must-revalidate --delete
